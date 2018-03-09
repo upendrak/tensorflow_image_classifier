922460182b02bdd136282ca5aa95493ee6bd75ef
upendrak <upendrakumar.devisetty@googlemail.com>
2018-03-09 08:14:00 -0700

    Add and modifiy files and folders

--- final.sh
+++ final.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python /classify_image.py --model_dir /model --image_file $1
+inputfile=$1
+
+outfile=$(basename $1 ".jpg")."out"
+
+python /classify_image.py --model_dir /model --image_file $inputfile > $outfile
